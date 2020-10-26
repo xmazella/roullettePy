6a94d4f5cf9dacffeaf3190a049f161931fcc7c3
Matteo Mervoyer <mmervoyer@yolaw.fr>
2020-10-26 16:16:27 +0100

    Comments

--- roullette.py
+++ roullette.py
@@ -22,8 +22,10 @@ param = myParam(36, 50)
 while True:
     os.system('clear')
     for bet in betList:
+        #print(f"you put {bet.tokenBet} chips on the number")
         print("you put", bet.tokenBet, "chips on the number",
               bet.num, "in a", bet.typeBet)
+    #print(f"\nYou have {param.token} tokens") --> pas besoin de faire str(param.token)
     print('\nYou have', str(param.token), 'tokens\n')
     if param.token > 0:
         print('press 1 for play single number')
@@ -37,6 +39,7 @@ while True:
     if playerChoise == 1 and param.token > 0:
         simple = int(input('Choose a number beetwen 1 and 36\n'))
         tokenbet = int(input('how much tokens do you want to bet\n'))
+        # 0 >= simple < 37
         if simple <= 0 or simple > 36 or (param.token - tokenbet) < 0:
             print('error')
             continue
@@ -45,6 +48,7 @@ while True:
     elif playerChoise == 2 and param.token > 0:
         cols = int(input('Choose a cols 1, 2 or 3\n')) - 1
         tokenbet = int(input('how much tokens do you want to bet\n'))
+        # 0 > cols < 3
         if cols < 0 or cols > 2 or (param.token - tokenbet) < 0:
             print('error')
             continue
@@ -53,6 +57,7 @@ while True:
     elif playerChoise == 3 and param.token > 0:
         tiers = int(input('Choose a tiers 1, 2 or 3\n')) - 1
         tokenbet = int(input('how much tokens do you want to bet\n'))
+        #meme logique qu'au dessus
         if tiers < 0 or tiers > 2 or (param.token - tokenbet) < 0:
             print('error')
             continue
@@ -66,26 +71,32 @@ while True:
         for bet in betList:
             if bet.typeBet == "simple bet":
                 if bet.num == b:
+                    #f string comme au dessus
                     print("you win", bet.tokenBet * 36, "chips on the number",
                           bet.num, "in a", bet.typeBet)
                     param.token = param.token + bet.tokenBet * 36
                 else:
+                    #f string comme au dessus
                     print("you lose", bet.tokenBet, "chips on the number",
                           bet.num, "in a", bet.typeBet)
             if bet.typeBet == "cols bet":
                 if bet.num == d:
+                    #f string comme au dessus
                     print("you win", bet.tokenBet * 2, "chips on the number",
                           bet.num, "in a", bet.typeBet)
                     param.token = param.token + bet.tokenBet * 2
                 else:
+                    #f string comme au dessus
                     print("you lose", bet.tokenBet, "chips on the number",
                           bet.num, "in a", bet.typeBet)
             if bet.typeBet == "tiers bet":
                 if bet.num == c:
+                    #f string comme au dessus
                     print("you win", bet.tokenBet * 2, "chips on the number",
                           bet.num, "in a", bet.typeBet)
                     param.token = param.token + bet.tokenBet * 2
                 else:
+                    #f string comme au dessus
                     print("you lose", bet.tokenBet, "chips on the number",
                           bet.num, "in a", bet.typeBet)
         input()
